3f796ac1b7034b8ef4e211d00d254c42760baf7a
Jota <jota.zehering@gmail.com>
2024-08-14 22:31:08 -0300

    projeto exemplo tipos primitivos - variaveis e constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,39 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome = "Jota";
+
+        System.out.println(meuNome);
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+        /*
+		 * esta linha é considerada como declaração de variável iniciamos a existência
+		 * variavel numero com valor 5 regra: tipo + nome + valor
+		 */
+        int numero = 5;
+
+        /*
+		 * na linha abaixo iremos alterar o valor do varíavel para 10 observe que o tipo
+		 * não é mais necessário, pois a variável já foi declarada anteriormente
+		 */
+        numero = 10;
+
+        System.out.println(numero);
+
+        /*
+		 * ao usar a palavra reservada final, você determina que jamais
+		 * esta variavel poderá obter outro valor;
+		 * logo a linha 25 vai apresentar um erro de compilação
+		 * isso é considerado uma CONSTANTE na linguagem Java
+		 */
+		final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
+
     }
 }
