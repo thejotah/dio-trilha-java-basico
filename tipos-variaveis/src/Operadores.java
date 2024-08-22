eb7fe83687957285ce6738800c70ee37f38a3fa3
Jota <jota.zehering@gmail.com>
2024-08-22 16:59:03 -0300

    praticando operadores 2

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,12 +2,25 @@ public class Operadores {
     public static void main(String[] args) {
         int numero = 5;
 
-        numero = - numero;
+       //x repetição
+       numero++;
+       //numero = numero +1;
 
-        System.out.println(numero);
+       System.out.println(numero);
 
-        numero = numero * -1;
+       System.out.println(numero++);
+       //primeiro imprimir / depois acrescentar +1
 
-        System.out.println(numero);
+       System.out.println(++numero);
+       //acrescentar +1 / depois imprimir com o resultado
+
+       System.out.println(numero);
+
+       System.out.println(numero--);
+       //restirar -1 / depois imprimir com o resultado
+
+       System.out.println(numero);
+
+
     }
 }
