baff24dbd0f2b06c05833e60be59f57949797b50
Jota <jota.zehering@gmail.com>
2024-08-22 18:10:01 -0300

    praticando operador ternario

---
+++ tipos-variaveis/src/OperadorTernario.java
@@ -0,0 +1,28 @@
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        //String resultado = "";
+        //if(a==b)
+        //    resultado = "verdadeiro";
+        //else
+        //    resultado = "falso";
+
+        //System.out.println(resultado);
+
+        //OPERADOR TERNÁRIO
+
+        //String resultado = a==b ?"verdadeiro" : "falso";
+        //String resultado sempre String
+
+        int resultado = a==b ? 1 : 0;
+        //int resultado sempre int
+
+        System.out.println(resultado);
+
+        //operador ternário não se limita a string
+    }
+}
